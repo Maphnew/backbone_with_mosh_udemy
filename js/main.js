eb615c91b74a1a046a1f048b7e0f7971d342506d
maphnew <zcm3118@gmail.com>
2021-03-28 23:41:59 +0900

    Model Project

--- js/main.js
+++ js/main.js
@@ -19,3 +19,48 @@ var Song = Backbone.Model.extend({
 var song = new Song({ songId: 1, title: "Happy"});
 
 
+var Vehicle = Backbone.Model.extend({
+    idAttribute:"registrationNumber",
+    urlRoot: "/api/vehicles",
+    start: function() {
+        console.log("Vehicle started")
+    },
+    validate: function(attrs) {
+        if(!attrs.registrationNumber) 
+        return "Registration Number is required."
+    }
+    
+})
+
+var Car = Vehicle.extend({
+    // idAttribute: Vehicle.prototype.idAttribute,
+    start: function() {
+        Vehicle.prototype.start.apply(this);
+        const regiNumber = this.get("registrationNumber")
+        
+        console.log(`Car with registration number ${regiNumber} started.`)
+    }
+});
+
+
+var car = new Car({ registrationNumber: "ABC" });
+car.get("registationNumber")
+car.start()
+
+var benz = new Car({
+    registrationNumber: "XLI887",
+    color: "Blue"
+});
+console.log(benz);
+benz.unset("registrationNumber")
+console.log(benz);
+var isValid = benz.isValid();
+var isError = benz.validationError;
+
+console.log(isValid, isError);
+
+benz.set("registrationNumber", "XLI887");
+
+var isValidNow = benz.isValid();
+var isErrorNow = benz.validationError;
+console.log(isValidNow, isErrorNow);
