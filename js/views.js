3a34899900b34620380f5ab6ad2f187fcd0a30b7
maphnew <zcm3118@gmail.com>
2021-04-04 23:07:53 +0900

    Vehicle PJT

--- js/views.js
+++ js/views.js
@@ -1,14 +1,40 @@
-var Song = Backbone.Model.extend();
-var SongView = Backbone.View.extend({
+var Vehicle = Backbone.Model.extend();
+var Vehicles = Backbone.Collection.extend({
+    model: Vehicle
+});
+var VehicleView = Backbone.View.extend({
+    tagName: "li",
+    className: "vehicle",
+    events: {
+        "click .deleteVehicle": "onVehicleDelete"
+    },
+    onVehicleDelete: function(vehicle) {
+        this.remove();
+    },
     render: function() {
-        var template = _.template($("#songTemplate").html());
-        var html = template(this.model.toJSON());
-        this.$el.html(html);
+        this.$el.html(this.model.get("registrationNumber") + "<button class='deleteVehicle'>Delete</button>")
         return this;
     }
 });
 
-var song = new Song({ title: "Blue in Green", plays: 1110 });
+var VehiclesView = Backbone.View.extend({
+    tagName: "ul",
+    render: function() {
+        var self = this;
+        this.model.each(function(vehicle){
+            var vehicleView = new VehicleView({ model: vehicle });
+            self.$el.append(vehicleView.render().$el);
+        });
+    }
+});
 
-var songView = new SongView({ el: "#container", model: song });
-songView.render();
+
+var vehicles = new Vehicles([
+    new Vehicle({ registrationNumber: "ABC123", "data-color": "red" }),
+    new Vehicle({ registrationNumber: "ZBV510", "data-color": "green" }),
+    new Vehicle({ registrationNumber: "CBC111", "data-color": "orange" })
+]);
+
+var vehiclesView = new VehiclesView({ el: "#vehicles", model: vehicles });
+
+vehiclesView.render();
